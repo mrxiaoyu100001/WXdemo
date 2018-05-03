01f52eb07c44073f5ab6fef24489a63f9fa81ca0
qianyu <qianyu@meicai.cn>
2018-05-03 19:41:12 +0800

    添加详情页

--- pages/main/main.js
+++ pages/main/main.js
@@ -49,13 +49,6 @@ Page({
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
@@ -64,39 +57,11 @@ Page({
       isShow: false
     })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onItemClick: function(e) {
+    console.log('backgroundClick', e)
+    console.log('id', e.target.dataset.id)
+    wx.navigateTo({
+      url: '/pages/details/index',
+    })
   }
 })
